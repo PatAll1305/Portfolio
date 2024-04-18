5f11b7f7288b3f91e9e3a95f8de2d28b04f1212b
Patrick Allen <patrickallen1305@gmail.com>
2024-04-17 21:28:31 -0500

    main mostly completed

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const resumeButton = document.querySelector(".resume")
+const githubButton = document.querySelector(".github")
+const achievementsButton = document.querySelector(".achievements")
 
 resumeButton.addEventListener("click", () => {
-
+    window.location.href = "http://localhost:5500/views/resume.html"
 })
+
+githubButton.addEventListener("click", () => {
+
+    window.location.href = "https://github.com/PatAll1305"
+})
+
+achievementsButton.addEventListener("click", () => {
+    window.location.href = "http://localhost:5500/views/achievements.html"
+}) 
